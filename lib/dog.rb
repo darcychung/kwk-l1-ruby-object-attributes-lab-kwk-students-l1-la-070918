51bf1298aeb1e13ff767092f21c42fb3e6379c5c
darcychung <darcycchung@gmail.com>
2018-07-12 22:29:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,20 +12,12 @@ def name= (new_name)
   @name= new_name
 end
 
-
- puts fido.name
-
 def breed
   @breed= "Beagle"
 end
 def breed= (new_breed)
   @breed= new_name
 end
-
-beagle= Dog.new
-beagle.breed = "Beagle"
-
-puts beagle.breed
 
 
     
