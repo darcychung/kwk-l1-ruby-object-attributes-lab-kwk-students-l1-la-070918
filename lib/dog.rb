0d1881d402e324cf2683ee63eee27795186e9a9d
darcychung <darcycchung@gmail.com>
2018-07-12 22:35:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,18 +5,18 @@ def initialize(name,breed)
 end 
 
 def name
-  @name= "Fido"
+  @name = "Fido"
 end
 
 def name=(new_name)
-  @name= new_name
+  @name = new_name
 end
 
 def breed
-  @breed= "Beagle"
+  @breed = "Beagle"
 end
-def breed= (new_breed)
-  @breed= new_breed
+def breed=(new_breed)
+  @breed = new_breed
 end
 end
 
