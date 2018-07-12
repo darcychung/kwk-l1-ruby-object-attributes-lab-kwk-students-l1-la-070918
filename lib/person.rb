496284e4d27b4b95a1ef4f964019a920461f0282
darcychung <darcycchung@gmail.com>
2018-07-12 22:31:03 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # person.rb
+class Person
+def initialize (name, job)
+  @name = name
+  @job = job
+end 
+
+def name
+  @name= "Beyonce"
+end
+
+def name= (new_name)
+  @name= new_name
+end
+
+def job
+  @job= "singer"
+end
+def singer= (new_job)
+  @job= new_job
+end
+
+
+    
